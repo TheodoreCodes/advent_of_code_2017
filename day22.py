c6ee9ba0f8b7c3cb5b008db426caa126f9e0355e
Theodore <tc_accounts@protonmail.com>
2018-02-20 22:37:07 +0000

    Cleaned up day 22 a bit

--- day22.py
+++ day22.py
@@ -6,36 +6,28 @@ for j, line in enumerate(open("inputs/22.in").readlines()):
     for i, v in enumerate(line.strip("\n")):
         grid[(i, j)] = 2 if v == '#' else 0
 
-initial = copy(grid)
-
-pos = (len(line) // 2, len(line) // 2)
-directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-cur_dir = 0
-
-infections = 0
-for _ in range(10_000):
-    cur_dir = (cur_dir + (-1 if grid[pos] == 0 else 1)) % 4
-    grid[pos] = 2 if grid[pos] == 0 else 0
-    infections += 1 if grid[pos] == 2 else 0
-    pos = (pos[0] + directions[cur_dir][0], pos[1] + directions[cur_dir][1])
-
-print("First part:", infections)
-
 '''
 0 - Clean
 1 - Weakend
 2 - Infected
 3 - Flagged
 '''
-grid = initial
-pos = (len(line) // 2, len(line) // 2)
-infections = 0
-cur_dir = 0
+def contaminate(gap, bursts):
+    local_grid = copy(grid)
 
-for _ in range(10_000_000):
-    cur_dir = (cur_dir + grid[pos] - 1) % 4
-    grid[pos] = (grid[pos] + 1) % 4
-    infections += 1 if grid[pos] == 2 else 0
-    pos = (pos[0] + directions[cur_dir][0], pos[1] + directions[cur_dir][1])
+    pos = (len(line) // 2, len(line) // 2)
+    directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    cur_dir = 0
+    infections = 0
 
-print("Second part:", infections)
+    for _ in range(bursts):
+        cur_dir = (cur_dir + local_grid[pos] - 1) % 4
+        local_grid[pos] = (local_grid[pos] + gap) % 4
+        infections += 1 if local_grid[pos] == 2 else 0
+        pos = (pos[0] + directions[cur_dir][0], pos[1] + directions[cur_dir][1])
+
+    return infections
+
+
+print("First part:", contaminate(2, 10_000))
+print("First part:", contaminate(1, 10_000_000))
